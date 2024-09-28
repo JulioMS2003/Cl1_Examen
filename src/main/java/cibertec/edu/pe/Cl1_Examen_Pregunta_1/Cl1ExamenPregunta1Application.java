7bfd349c01ca72d625e94683fe5bf0ed34a2b466
= <=>
2024-09-27 19:35:52 -0500

    first commit

---
+++ src/main/java/cibertec/edu/pe/Cl1_Examen_Pregunta_1/Cl1ExamenPregunta1Application.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.Cl1_Examen_Pregunta_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1ExamenPregunta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1ExamenPregunta1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/Cl1_Examen_Pregunta_1/Cl1ExamenPregunta1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.Cl1_Examen_Pregunta_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1ExamenPregunta1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
